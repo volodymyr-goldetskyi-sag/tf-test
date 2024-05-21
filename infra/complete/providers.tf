ca8e8e6ea1e1621d56ee54ee476c38939f26ba68
volodymyr-goldetskyi-sag <136732783+volodymyr-goldetskyi-sag@users.noreply.github.com>
2024-05-22 00:39:02 +0200

    Test branch-planner and flux tf k8s access

--- infra/complete/providers.tf
+++ infra/complete/providers.tf
@@ -10,20 +10,7 @@ provider "github" {
 }
 
 provider "flux" {
-  kubernetes = {
-    host                   = module.eks.eks_cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.eks_cluster_certificate_authority_data)
-    exec = {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args = [
-        "eks",
-        "get-token",
-        "--cluster-name",
-        var.eks_cluster_name,
-      ]
-    }
-  }
+  kubernetes = { }
   git = {
     url = "ssh://${var.flux_github_ssh_user}@${var.flux_github_url}/${var.flux_github_owner}/${var.flux_github_repository}.git"
     branch = var.flux_github_branch
